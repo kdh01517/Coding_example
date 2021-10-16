36743a1ef8b6ebc29ad480fb181339567f18ba4b
DONGHYUN KIM <kdh01517@naver.com>
2021-10-16 21:25:41 +0900

    9935

---
+++ stack/9935.py
@@ -0,0 +1,18 @@
+import sys
+
+a = sys.stdin.readline().strip()
+b = sys.stdin.readline().strip()
+
+arr=[]
+
+tick = len(b)
+
+for i in a:
+    arr.append(i)
+    if b[-1] == i and ''.join(arr[-tick:]) == b:
+        del arr[-tick:]
+
+if arr:
+    print(''.join(arr))
+else:
+    print('FRULA')
